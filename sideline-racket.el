ce7f58700f0bc13d315d8e4616ec7e58f26218de
Jen-Chieh Shen <jcs090218@gmail.com>
2025-02-05 15:45:18 -0800

    docs: Add commentary

--- sideline-racket.el
+++ sideline-racket.el
@@ -28,6 +28,14 @@
 ;;
 ;; Show racket result with sideline.
 ;;
+;; 1) Add sideline-racket to sideline backends list,
+;;
+;;   (setq sideline-backends-right '(sideline-racket))
+;;
+;; 2) Then enable sideline-mode in the target buffer,
+;;
+;;   M-x sideline-mode
+;;
 
 ;;; Code:
 
